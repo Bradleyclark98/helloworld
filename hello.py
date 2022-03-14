133f16923f75850fb9336d3736ffec727a5d31a5
Bradleyclark98 <bradley.clark@peak.ai>
2022-03-14 08:17:42 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-print ("Hello Peak!")
- 
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
+end_date = start_date + relativedelta(months=delta_period) + timedelta(days=-delta_period)
 
   
